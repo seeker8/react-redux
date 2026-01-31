8a2892b64e5c2f77395ebef7a7121e99194bd379
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-31 13:58:56 -0500

    match course version to go over enzyme section

--- src/components/tests/App.spec.js
+++ src/components/tests/App.spec.js
@@ -1,7 +1,10 @@
-import {render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { App } from '../App';
 
 test('CommentBox is rendered',  () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/CommentBox/i)).toBeInTheDocument();
+    const container = document.createElement('div');
+    ReactDOM.render(<App />, container);
+    expect(container.innerHTML).toContain('CommentBox component');
+    ReactDOM.unmountComponentAtNode(container);
 });
