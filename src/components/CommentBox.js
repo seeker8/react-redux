7a0cf6df690423411405cf28be9aa9e09f974211
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-31 15:07:34 -0500

    refactor the CommentBox componet to be a class component and modify the jsx to show the textarea and submit button

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-export const CommentBox = () => {
-    return (
-        <div>CommentBox component</div>
-    );
-};
+export class CommentBox extends React.Component {
+    state = { comment: '' };
+
+    handleChange = (e) => {
+        this.setState({ comment: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({ comment: '' });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a comment</h4>
+                <textarea value={this.state.comment} onChange={this.handleChange}/>
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        );
+    }
+}
