27c0b749792348f8458bf912d8c390c4424955d6
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-31 18:14:18 -0500

    add the new action type to our commentsReducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,9 @@
+import { SAVE_COMMENT } from "../actions/types";
+
 export const commentsReducer = (state = [], action) => {
     switch (action.type) {
+        case SAVE_COMMENT:
+            return [...state, action.payload];
         default:
             return state;
     }
