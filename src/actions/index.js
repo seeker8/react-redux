9855fa91329b4c430832962bf250c52b9ca4ec15
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-01 12:13:25 -0500

    add fetch action createor

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
 import { SAVE_COMMENT } from "./types";
+import { FETCH_COMMENTS } from "./types";
+import axios from "axios";
 
 export const saveComment = (comment) => {
     return {
         type: SAVE_COMMENT,
         payload: comment
+    }
+}
+
+export const fetchComments = () => {
+    return {
+        type: FETCH_COMMENTS,
+        payload: axios.get('https://jsonplaceholder.typicode.com/comments')
+            .then(response => response.json())
+            .catch(error => console.log(error))
     }
 }
