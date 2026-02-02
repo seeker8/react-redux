e8d13da4f08687c645ec181bcb92337d2352ba89
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-02 14:58:28 -0500

    inital route setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { CommentBox } from './CommentBox';
 import { CommentList } from './CommnentList';
 
 export const App = () => {
     return (
         <div>
-            <CommentBox />
-            <CommentList />
+            <Route
+                path="/post" component={CommentBox} />
+            <Route
+                path="/" exact component={CommentList} />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 import { Root } from './Root';
 
 ReactDOM.render((
     <Root>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Root>
 ), document.getElementById('root'));
