d55dc556f72ab525b58a6064b63831de0b94fd41
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-02 13:02:43 -0500

    fix messages structure to match what's expected by the reducer and complete test with expect

--- src/tests/comments-integration.spec.js
+++ src/tests/comments-integration.spec.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 test('can fetch a list of comments and display them', () => {
     axios.get = jest.fn().mockReturnValue({
-        data: ['Comment 1', 'Comment 2', 'Comment 3']
+        data: [{name: 'Comment 1'}, {name: 'Comment 2'}, {name: 'Comment 3'}]
     });
     const wrapper = mount(
         <Root>
@@ -15,4 +15,5 @@ test('can fetch a list of comments and display them', () => {
         </Root>
     );
     wrapper.find('button[data-testid="fetch-button"]').simulate('click');
+    expect(wrapper.find('li').length).toEqual(3);
 });
