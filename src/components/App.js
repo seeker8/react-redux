4659a596565f8576ad5ba0b2066314e5780436d5
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-02 16:11:32 -0500

    add navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions/index';
+import { Route, Link } from 'react-router-dom';
 import { CommentBox } from './CommentBox';
 import { CommentList } from './CommnentList';
 
+const NavigationComponent = ({ auth }) => {
+  return (
+    <nav>
+      <ul style={{ "padding-left": 0 }}>
+        <li style={{ display: "inline-block", padding: "0 5px" }}>
+          <Link to="/">Home</Link>
+        </li>
+        <li style={{ display: "inline-block", padding: "0 5px" }}>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li style={{ display: "inline-block", padding: "0 5px" }}>
+          {(auth
+            && <Link to="/auth">Sign Out</Link>)
+            || <Link to="/auth">Sign In</Link>}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const headerStateToProps = (state) => {
+  return { auth: state.auth };
+}
+
+const Navigation = connect(headerStateToProps, actions)(NavigationComponent);
+
 export const App = () => {
-    return (
-        <div>
-            <Route
-                path="/post" component={CommentBox} />
-            <Route
-                path="/" exact component={CommentList} />
-        </div>
-    )
+  return (
+    <div>
+      <Navigation />
+      <Route
+        path="/post" component={CommentBox} />
+      <Route
+        path="/" exact component={CommentList} />
+    </div>
+  )
 }
